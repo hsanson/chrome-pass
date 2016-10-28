0fbfad16ea8148fa29da25127d079acf214327e5
Horacio Sanson <horacio@allm.net>
2016-10-28 23:05:01 +0900

    Bump version to 0.1.6

--- application/setup.py
+++ application/setup.py
@@ -16,7 +16,7 @@ class install_native_app(install):
 
 setup(
   name="chrome-pass",
-  version="0.1.5",
+  version="0.1.6",
   description="Chrome Native application for pass - the standard Unix password manager",
   url="https://github.com/hsanson/chrome-pass",
   author="Horacio Sanson",
