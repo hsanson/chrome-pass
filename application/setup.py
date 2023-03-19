240e16547e0598c034465f8bf9838f1531e1ee4e
Horacio Sanson <horacio@allm.inc>
2023-03-19 16:24:58 +0900

    Revert "Replace distutils with setuptools"
    This reverts commit 1a8f57f0948bcf0d24f878886c288f868104595e.
    
    This change broke nativePass install command. Seems distutils generates
    some binary that uses pkg_resources to actually execute the script. This
    results in the os.path.realpath() function returning some obscure
    egg/wheel paths that do not actually exist.
    
    The setuptools method works fine so reverting.

--- application/setup.py
+++ application/setup.py
@@ -1,39 +1,35 @@
-"""
-Release
-
-pip3 install --user twine
-Update the version string below.
-Build package with `python3 setup.py sdist`
-Upload package with `twine upload dist/*`
-"""
+# Release
+#
+# pip3 install --user twine
+# Update the version string below.
+# Build package with `python3 setup.py sdist`
+# Upload package with `twine upload dist/*`
 
 import sys
-from setuptools import setup
-from setuptools.command.install import install
+import os
+import subprocess
+from distutils.core import setup
+from distutils.command.install import install
 
 if sys.version_info[0] < 3:
-    sys.stderr.write('Only Python 3 supported\n')
-    sys.exit(1)
+  sys.stderr.write('Only Python 3 supported\n')
+  sys.exit(1)
 
+class install_native_app(install):
 
-class InstallNativeApp(install):
-
-    def run(self):
-        self.do_egg_install()
-        print("Execute `nativePass install` to install the native"
-              "host application")
-
+  def run(self):
+    super().run()
+    print("Execute nativePass install now to install the native host application")
 
 setup(
   name="chrome-pass",
   version="0.3.0",
-  description="Chrome Native application for pass -"
-              "the standard Unix password manager",
+  description="Chrome Native application for pass - the standard Unix password manager",
   url="https://github.com/hsanson/chrome-pass",
   author="Horacio Sanson",
   author_email="e@e.com",
   license="MIT",
   install_requires=['python-gnupg'],
   scripts=["nativePass"],
-  cmdclass={'install': InstallNativeApp}
+  cmdclass={ 'install': install_native_app }
   )
